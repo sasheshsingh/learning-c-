d37222c64fc82b810afaaf330e6327ad15a08226
Sashesh Singh <sashesh.sss@gmail.com>
2025-02-18 12:30:34 +0530

    day 6: learning functions

---
+++ Day06/02_function2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+// Call by Referebce
+void swap(int *a, int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+int main(){
+    int a=10;
+    int b=20;
+    cout<<"Before swapping"<<endl;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    swap(&a,&b);
+    cout<<"After swapping"<<endl;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    return 0;
+}
